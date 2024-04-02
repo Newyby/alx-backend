48d4841ef89abc94acbe11606204ed231a163b38
Akinsanmi omobonike <officialraydata@gmail.com>
2024-04-02 18:49:41 +0000

    Updated code

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,57 +1,39 @@
 #!/usr/bin/env python3
-
-
+"""Most Recently Used caching module.
 """
-This module illustrates how to do simple caching
-in python. This time, we will do FIFO caching.
-If the cache is at maximum capacity, we will discard
-the first item in and add the new one (FIFO)
-"""
-
 from collections import OrderedDict
-BaseCaching = __import__("base_caching").BaseCaching
+
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limit is reached.
     """
-    A prety basic caching  system implementation
-    Based on a FIFO list
-    """
-
     def __init__(self):
-        """
-        The constructor
+        """Initializes the cache.
         """
         super().__init__()
-        self.cache_data = OrderedDict(self.cache_data)
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        """Adds an item in the cache.
         """
-        Add an item to the cache
-        Args:
-            key (str): the key for the cache
-            item (str): the value to se
-        Returns:
-            Returns None (explicitly)
-        """
-
-        if len(self.cache_data.keys()) >= super().MAX_ITEMS:
-            last_key = list(self.cache_data.keys())[0]
-            self.cache_data.pop(last_key)
-            print(f'DISCARD: {last_key}')
-        if key and item:
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
             self.cache_data[key] = item
-            # self.cache_data.move_to_end(key, last=False)
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
+        """Retrieves an item by key.
         """
-        Let's get the cache item by the key
-        Args:
-            key (str): Key to the item
-        Returns:
-            if key in self.cached_items, return the
-            item, None otherwise
-        """
-        if key in self.cache_data.keys():
-            self.cache_data.move_to_end(key, last=True)
-        return self.cache_data.get(key)
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
